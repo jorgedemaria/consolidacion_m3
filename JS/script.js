1d87b16c8fd8015c4a5aff53235f333db183d71f
Jorge de María <jorgedemaria@pm.me>
2024-07-29 11:58:37 -0400

    final final final

--- JS/script.js
+++ JS/script.js
@@ -31,23 +31,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// function currentTime(){
-// let date = new Date();
-// let hh = date.getHours();
-// let mm = date.getMinutes();
-// let ss = date.getSeconds();
-
-// hh = (hh < 10) ? "0" + hh : hh;
-// mm = (mm < 10) ? "0" + mm : mm;
-// ss = (ss < 10) ? "0" + ss : ss;
-
-// let time = hh + ":" + mm + ":" + ss;
-// let watch = document.querySelector("#watch");
-// watch.innerHTML = time;
-// }
-
-// setInterval(currentTime, 1000)
-
 const $time = document.querySelector('.time'),
       $date = document.querySelector('.date');
 
