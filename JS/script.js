ada97c05f11c84c49e345ecba23b031da36a0b38
Jorge de María <jorgedemaria@pm.me>
2024-07-29 10:05:00 -0400

    hola

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const items = document.querySelectorAll(".chat-list__item");
-    const chatRight = document.querySelector(".chat-right");
-
-
-    items.forEach(item => {
-        const itemName = item.querySelector(".chat-list__name h5").textContent;
-
-        item.addEventListener("click", () => {
-            if (itemName === "Emma") {
-                chatRight.style.display = 'flex'; 
-            } else {
-                chatRight.style.display = 'none';
-            }
-            if (!item.classList.contains("clicked")) {
-                item.classList.add("clicked");
-            }
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    const chatEmma = document.getElementById('chat-info-emma');
+    const chatRight = document.querySelector('.chat-right');
+    const chatItems = document.querySelectorAll('.chat-list__item');
+  
+    function handleChatItemClick(event) {
+      event.currentTarget.classList.add('clicked');
+  
+      if (event.currentTarget === chatEmma) {
+        chatRight.classList.remove('d-none');
+      } else {
+        chatRight.classList.add('d-none');
+      }
+    }
+  
+    chatItems.forEach(item => {
+      item.addEventListener('click', handleChatItemClick);
     });
-
-});
+  
+    chatEmma.classList.add('clicked');
+    chatRight.classList.remove('d-none');
+  
+    window.addEventListener('resize', function () {
+      if (window.innerWidth < 992) {
+        chatRight.classList.remove('d-none');
+      } else {
+        if (!chatEmma.classList.contains('clicked')) {
+          chatRight.classList.add('d-none');
+        }
+      }
+    });
+  });
